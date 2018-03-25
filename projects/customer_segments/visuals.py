8ecc002c8f35c3f9c85e411c6ffbc2f0abb6abd5
Lars Jaakko Havro <larsjh@gmail.com>
2018-03-25 12:19:34 +0200

    Updates to customer segment project

--- projects/customer_segments/visuals.py
+++ projects/customer_segments/visuals.py
@@ -62,7 +62,7 @@ def cluster_results(reduced_data, preds, centers, pca_samples):
 	fig, ax = plt.subplots(figsize = (14,8))
 
 	# Color map
-	cmap = cm.get_cmap('gist_rainbow')
+	cmap = cm.get_cmap('Paired')
 
 	# Color the points based on assigned cluster
 	for i, cluster in plot_data.groupby('Cluster'):   
